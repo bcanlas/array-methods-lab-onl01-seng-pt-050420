a966172db624d4784f16887efac64a3a74b9e7be
Bianca Canlas <bcanlas87@gmail.com>
2020-05-14 01:52:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,6 @@
 def using_include(array, element)
-  round_things=["ball", "sun", "orange"]
-  round_things.include?("sun")
+  array.include?(element)
 end
-def using_include(array, element)
-  round_things=["ball", "sun", "orange"]
-  round_things.include?("moon")
-end  
 
 def using_sort(array)
 
